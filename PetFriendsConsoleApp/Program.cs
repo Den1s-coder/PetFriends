75b5eb2fd73e7c06f5cc932c5263550b375c1abb
Denis Katasonov <diarusfoxs@gmail.com>
2024-09-27 18:11:36 +0300

    Update: Program.cs

--- PetFriendsConsoleApp/Program.cs
+++ PetFriendsConsoleApp/Program.cs
@@ -1,7 +1,35 @@
-﻿internal class Program
+﻿using PetFriendsConsoleApp.Entity;
+using PetFriendsConsoleApp.Methods;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Pet[] pets = new Pet[8];
+        pets[0] = new Pet("Cat", "Jogh", 4);
+
+        while (true){
+            int number;
+
+            Console.Write("Выберете задачу: ");
+
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Неверный формат возраста. Введите число:");
+            };
+
+            switch (number)
+            {
+                case 1:
+                    PetService.GetAllPets(pets);
+                    break;
+                case 2:
+                    PetService.AddPet(pets);
+                    break;
+                case 3:
+                    PetService.EditPetInfo(pets);
+                    break;
+            }
+        }
     }
 }
