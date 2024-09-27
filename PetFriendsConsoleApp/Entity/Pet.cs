1e8a55f2ee4a7a4f471816de1425470a92bde96c
Denis Katasonov <diarusfoxs@gmail.com>
2024-09-27 18:08:20 +0300

    Update: delete Id property

--- PetFriendsConsoleApp/Entity/Pet.cs
+++ PetFriendsConsoleApp/Entity/Pet.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace PetFriendsConsoleApp.Entity
 {
-    internal class Cat
+    internal class Pet : IPet
     {
+        public string PetType {  get; set; }
+        public string Name { get; set; }
+        public int Age {  get; set; }
+        public string PhysicalDescription { get; set; }
+        public string PersonalityDescription { get; set; }
+
+        public Pet(string PetType, string Name, int Age, string PhysicalDescription = "Nothing", string PersonalityDescription = "Nothing")
+        {
+            this.PetType = PetType;
+            this.Name = Name;
+            this.Age = Age;
+            this.PhysicalDescription = PhysicalDescription;
+            this.PersonalityDescription = PersonalityDescription;
+        }
+
+        public void PetInfo()
+        {
+            Console.WriteLine($"{PetType} named {Name} she is {Age} years old");
+            Console.WriteLine($"Desription\n Physical: {PhysicalDescription}.\n Personality: {PersonalityDescription}.\n");
+        }
     }
 }
