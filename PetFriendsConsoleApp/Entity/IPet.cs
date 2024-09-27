48385c538caa5c44408550651cc00d7ea77ff55f
Denis Katasonov <diarusfoxs@gmail.com>
2024-09-27 18:07:46 +0300

    Update: IPet delete Id property

--- PetFriendsConsoleApp/Entity/IPet.cs
+++ PetFriendsConsoleApp/Entity/IPet.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PetFriendsConsoleApp
+namespace PetFriendsConsoleApp.Entity
 {
-    internal interface Animal
+    internal interface IPet
     {
+        public string PetType { get; set; }
+        string Name { get; set; }
+        int Age { get; set; }
+        string PhysicalDescription { get; set; }
+        string PersonalityDescription { get; set; }
+
+        public void PetInfo();
     }
 }
