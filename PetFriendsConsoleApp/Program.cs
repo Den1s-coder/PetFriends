d3b0a121c0f60f29d332ddebbe70c5bfc9a575ea
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-01 17:00:27 +0300

    Add: Delete task and Exit task.

--- PetFriendsConsoleApp/Program.cs
+++ PetFriendsConsoleApp/Program.cs
@@ -5,17 +5,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Pet[] pets = new Pet[8];
-        pets[0] = new Pet("Cat", "Jogh", 4);
+        List<Pet> pets = new List<Pet>();
+        pets.Add(new Pet("Cat", "Jogh", 4));
 
         while (true){
             int number;
-
-            Console.Write("Выберете задачу: ");
+            Console.WriteLine("1. List all of our current pet information.\n2. Add a new animal friend to the list. \n3. Change pet information.\n4. Delete pet from list.\n5. Exit");
+            Console.Write("Choose a task number - ");
 
             while (!int.TryParse(Console.ReadLine(), out number))
             {
-                Console.WriteLine("Неверный формат возраста. Введите число:");
+                Console.WriteLine("Error: Incorrect format. Enter a number:");
             };
 
             switch (number)
@@ -29,6 +29,11 @@ internal class Program
                 case 3:
                     PetService.EditPetInfo(pets);
                     break;
+                case 4:
+                    PetService.DeletePet(pets);
+                    break;
+                case 5:
+                    return;
             }
         }
     }
