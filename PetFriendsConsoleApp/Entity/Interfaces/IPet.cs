2e286067b2b5ab23a6fe439b5fd48361c69d8a7c
Denis Katasonov <diarusfoxs@gmail.com>
2024-10-01 16:03:26 +0300

    Add: interface directory

--- PetFriendsConsoleApp/Entity/Interfaces/IPet.cs
+++ PetFriendsConsoleApp/Entity/Interfaces/IPet.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PetFriendsConsoleApp.Entity
+namespace PetFriendsConsoleApp.Entity.Interfaces
 {
     internal interface IPet
     {
--- PetFriendsConsoleApp/Entity/Pet.cs
+++ PetFriendsConsoleApp/Entity/Pet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PetFriendsConsoleApp.Entity.Interfaces;
 
 namespace PetFriendsConsoleApp.Entity
 {
